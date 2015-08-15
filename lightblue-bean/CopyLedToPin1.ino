265c56b50f0223928be8a592ce11428abd782abc
Pascal Bauermeister <pbauermeister@users.noreply.github.com>
2015-08-15 15:03:41 +0200

    Update CopyLedToPin1.ino

--- lightblue-bean/CopyLedToPin1.ino
+++ lightblue-bean/CopyLedToPin1.ino
@@ -8,20 +8,20 @@
 ** Project: http://www.instructables.com/id/bluetooth-police-beacon
 */
 
-#define PIN 1
+#define PIN 1  // Pin driving MOSFET
 
 void setup() {
     pinMode(PIN, OUTPUT);
 }
 
-int cnt; // counter to blip the LED
+int cnt = 0; // counter to blip the LED
 
 void loop() {
-    uint8_t value = Bean.getLedRed();        // Read red component of LED
-    digitalWrite(PIN, value?HIGH:LOW);       // Drive pin high if R is not zero
+    uint8_t value = Bean.getLedRed();        // Read red value R of LED
+    digitalWrite(PIN, value?HIGH:LOW);       // Drive high if R not zero
 
-    Bean.setLedBlue((cnt++&15)==1 ? 5 : 0);  // This blips the LED's
-    Bean.setLedBlue(0);                      // blue component
+    Bean.setLedBlue((cnt++&15)==1 ? 5 : 0);  // This blips the LED's blue
+    Bean.setLedBlue(0);                      //   value
 
     Bean.sleep(50);
 }
